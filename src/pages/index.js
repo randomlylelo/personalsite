c1b2b1de08ab6033a0e3d87eea7587da6de4356c
Leo <38267386+randomlylelo@users.noreply.github.com>
2021-05-14 10:35:33 -0400

    navbar spacing.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,42 @@
-import * as React from "react"
+import * as React from 'react';
 
 const IndexPage = () => {
   return (
     <main>
-      <div className="bg-customGray text-white px-4">
-        {/* Navbar */}
-        <div className="flex">
-          {/* Title */}
-          <div className="uppercase text-xl">
-            Leo Zhang.
-          </div>
-          <div className="flex-grow"></div>
-          {/* Navbar items */}
-          <div className="flex items-center">
-            <div className="">
-              Portfilo
-            </div>
-            <div className="">
-              Contact
+      <div className="bg-customGray text-white">
+        <div className="w-9/12 m-auto">
+          {/* Navbar */}
+          <div className="flex py-8">
+            {/* Title */}
+            <div className="uppercase text-xl">Leo Zhang.</div>
+            <div className="flex-grow"></div>
+            {/* Navbar items */}
+            <div className="flex items-center">
+              <div className="px-4">Portfilo</div>
+              <div className="pl-4">Contact</div>
             </div>
           </div>
+          {/* Space */}
+          <div className=" pt-8"></div>
+          {/* Hero */}
+          <div className=" w-9/12 m-auto flex flex-col">
+            <div className="w-9/12">
+              <p className="text-6xl">
+                Hello! I'm Leo, an aspiring Software Developer.
+              </p>
+            </div>
+            <div className=" w-7/12 self-end">
+              <p className="text-large text-right">
+                Born in New York, raised in Miami. Currently a freshman at Caltech
+                seeking a Computer Science degree.
+              </p>
+            </div>
+          </div>
+          {/* Featured Projects */}
         </div>
-        {/* Hero */}
-        {/* Featured Projects */}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
